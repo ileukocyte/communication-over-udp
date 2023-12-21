01899f95f6f52d62720a05989be5c55c0334be40
ileukocyte <alex.ilakeful@gmail.com>
2023-12-21 21:07:05 +0100

    documentation added

--- main.py
+++ main.py
@@ -1,6 +1,11 @@
 from client import client
 from server import server
 
+# 1. The node switching logic sucks since it's based on lots of code duplication
+# 2. The server side has no adequate option to terminate the session for now except for stopping the process
+# 3. No optimal ARQ has been implemented even though I intended to use Go-Back-N instead of Stop and Wait
+# 4. The timeout system is not completely implemented
+#    (the client will be continuously resending messages unless manually terminated)
 if __name__ == "__main__":
     while True:
         print("Select the mode or type in \"EXIT\" to terminate the session:")
